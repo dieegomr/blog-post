9a2c2dd25ed5cf86536f7f705c1d782363926666
dieegomr <dmendes.rocha@gmail.com>
2024-04-04 19:02:58 -0300

    refactor: create PostsForm

---
+++ src/components/PostsForm.tsx
@@ -0,0 +1,94 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import usePosts from '../hooks/usePosts';
+import { Post } from '../types/post';
+import useModal from '../hooks/useModal';
+
+type FormFields = {
+  title: string;
+  body: string;
+};
+
+type PostsFormProps = {
+  posts: Post[];
+};
+
+export default function PostsForm({ posts }: PostsFormProps) {
+  const { register, handleSubmit, formState, reset } = useForm<FormFields>();
+  const { addPost, isSubmitting } = usePosts();
+  const { closeModal } = useModal();
+
+  const titlesArray = posts.map((post) => post.title);
+
+  const onSubmit: SubmitHandler<FormFields> = async (newPost: FormFields) => {
+    await addPost(newPost);
+    closeModal();
+    reset();
+  };
+
+  return (
+    <form
+      className="flex flex-col w-full px-20 items-center pt-6"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      {!isSubmitting ? (
+        <>
+          <input
+            id="title"
+            className="p-3 w-full shadow-sm outline-none"
+            placeholder="Título"
+            {...register('title', {
+              required: 'Título é obrigatório',
+              validate: (value) => {
+                if (titlesArray.includes(value)) {
+                  return 'Já existe uma postagem com esse título.';
+                }
+
+                return true;
+              },
+            })}
+          />
+          <div className="flex justify-start w-full h-8 pt-1">
+            {formState.errors.title && (
+              <span className="text-red-500 text-sm">
+                {formState.errors.title.message}
+              </span>
+            )}
+          </div>
+          <textarea
+            className="p-3 w-full h-28 shadow-sm outline-none"
+            placeholder="Conteúdo"
+            {...register('body', {
+              required: 'Conteúdo é obrigatório',
+              minLength: {
+                value: 5,
+                message: 'Conteúdo deve ter no mínimo 5 caracteres',
+              },
+            })}
+            id="body"
+          ></textarea>
+          <div className="flex justify-start w-full h-8 pt-1">
+            {formState.errors.body && (
+              <span className="text-red-500 text-sm">
+                {formState.errors.body.message}
+              </span>
+            )}
+          </div>
+        </>
+      ) : (
+        <div className="flex h-52 justify-center items-center">
+          <p>Adicionando...</p>
+        </div>
+      )}
+      <div className="flex justify-end w-full">
+        <button
+          disabled={isSubmitting}
+          className={`bg-cyan-600 hover:bg-cyan-700 text-white px-4 py-2 rounded ${
+            isSubmitting ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+        >
+          Adicionar
+        </button>
+      </div>
+    </form>
+  );
+}
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -3,26 +3,12 @@ import MessageCard from '../components/MessageCard';
 import Modal from 'react-modal';
 import useModal from '../hooks/useModal';
 import CloseIcon from '../components/CloseIcon';
-import { SubmitHandler, useForm } from 'react-hook-form';
 import usePosts from '../hooks/usePosts';
-
-type FormFields = {
-  title: string;
-  body: string;
-};
+import PostsForm from '../components/PostsForm';
 
 export default function Main() {
   const { isModalOpen, openModal, closeModal } = useModal();
-  const { register, handleSubmit, formState, reset } = useForm<FormFields>();
-  const { posts, isFetching, fetchError, isSubmitting, addPost } = usePosts();
-
-  const titlesArray = posts.map((post) => post.title);
-
-  const onSubmit: SubmitHandler<FormFields> = async (newPost: FormFields) => {
-    await addPost(newPost);
-    closeModal();
-    reset();
-  };
+  const { posts, isFetching, fetchError } = usePosts();
 
   if (isFetching) {
     return (
@@ -57,70 +43,7 @@ export default function Main() {
             </div>
           </div>
           <div className="flex flex-col justify-center items-center">
-            <form
-              className="flex flex-col w-full px-20 items-center pt-6"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              {!isSubmitting ? (
-                <>
-                  <input
-                    id="title"
-                    className="p-3 w-full shadow-sm outline-none"
-                    placeholder="Título"
-                    {...register('title', {
-                      required: 'Título é obrigatório',
-                      validate: (value) => {
-                        if (titlesArray.includes(value)) {
-                          return 'Já existe uma postagem com esse título.';
-                        }
-
-                        return true;
-                      },
-                    })}
-                  />
-                  <div className="flex justify-start w-full h-8 pt-1">
-                    {formState.errors.title && (
-                      <span className="text-red-500 text-sm">
-                        {formState.errors.title.message}
-                      </span>
-                    )}
-                  </div>
-                  <textarea
-                    className="p-3 w-full h-28 shadow-sm outline-none"
-                    placeholder="Conteúdo"
-                    {...register('body', {
-                      required: 'Conteúdo é obrigatório',
-                      minLength: {
-                        value: 5,
-                        message: 'Conteúdo deve ter no mínimo 5 caracteres',
-                      },
-                    })}
-                    id="body"
-                  ></textarea>
-                  <div className="flex justify-start w-full h-8 pt-1">
-                    {formState.errors.body && (
-                      <span className="text-red-500 text-sm">
-                        {formState.errors.body.message}
-                      </span>
-                    )}
-                  </div>
-                </>
-              ) : (
-                <div className="flex h-52 justify-center items-center">
-                  <p>Adicionando...</p>
-                </div>
-              )}
-              <div className="flex justify-end w-full">
-                <button
-                  disabled={isSubmitting}
-                  className={`bg-cyan-600 hover:bg-cyan-700 text-white px-4 py-2 rounded ${
-                    isSubmitting ? 'opacity-50 cursor-not-allowed' : ''
-                  }`}
-                >
-                  Adicionar
-                </button>
-              </div>
-            </form>
+            <PostsForm posts={posts} />
           </div>
         </div>
       </Modal>
