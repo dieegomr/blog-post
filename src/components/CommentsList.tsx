65539fd6d78024862e8fe6653af94fe8320c4342
dieegomr <dmendes.rocha@gmail.com>
2024-04-04 21:54:10 -0300

    refactor: put all possible renders inside comments

--- src/components/CommentsList.tsx
+++ src/components/CommentsList.tsx
@@ -4,18 +4,38 @@ import MessageCard from './MessageCard';
 
 type CommentsListProps = {
   comments: Comment[];
+  isLoadingComments: boolean;
+  commentsError: string;
 };
 
-export default function CommentsList({ comments }: CommentsListProps) {
+export default function CommentsList({
+  comments,
+  commentsError,
+  isLoadingComments,
+}: CommentsListProps) {
   return (
-    <div className="flex-col pt-4 pr-10 pl-10 pb-10">
-      {comments.length > 0 ? (
-        comments.map((comment) => (
-          <CommentItem key={comment.id} comment={comment} />
-        ))
-      ) : (
-        <MessageCard message={'Essa postagem não possui comentários'} />
+    <>
+      {!isLoadingComments && !commentsError && (
+        <div className="flex-col pt-4 pr-10 pl-10 pb-10">
+          {comments.length > 0 ? (
+            comments.map((comment) => (
+              <CommentItem key={comment.id} comment={comment} />
+            ))
+          ) : (
+            <MessageCard message={'Essa postagem não possui comentários'} />
+          )}
+        </div>
       )}
-    </div>
+      {isLoadingComments && (
+        <div className="flex h-96 w-full items-center justify-center">
+          <p>Carregando comentários...</p>
+        </div>
+      )}
+      {commentsError && (
+        <div className="flex h-96 w-full items-center justify-center">
+          <p>{commentsError}</p>
+        </div>
+      )}
+    </>
   );
 }
